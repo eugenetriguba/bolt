7a659ccce3857ef9150c6a5c415a0340d967d260
Eugene Triguba <eugenetriguba@gmail.com>
2024-03-14 21:16:36 -0400

    Release v0.7.0

--- internal/commands/version.go
+++ internal/commands/version.go
@@ -32,7 +32,7 @@ func (cmd *VersionCmd) Execute(
 	_ ...interface{},
 ) subcommands.ExitStatus {
 	consoleOutputter := output.NewConsoleOutputter()
-	err := consoleOutputter.Output("bolt v0.6.0")
+	err := consoleOutputter.Output("bolt v0.7.0")
 	if err != nil {
 		consoleOutputter.Error(err)
 		return subcommands.ExitFailure
